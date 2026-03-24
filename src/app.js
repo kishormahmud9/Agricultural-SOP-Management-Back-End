7bdcfe639f4e1deb4ee116ea549fee49010fa87c
NKRifat47 <nk.rifat47@gmail.com>
2026-03-25 01:22:20 +0600

    pdf download as .pdf extension 3

--- src/app.js
+++ src/app.js
@@ -24,6 +24,7 @@ app.use(
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    exposedHeaders: ["Content-Disposition"],
   }),
 );
 
